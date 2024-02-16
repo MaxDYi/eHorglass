d5db1a9e65b5f114ee35bd8a080525fe5e94aa20
MaxDYi <1575966435@QQ.com>
2024-02-16 10:40:16 +0800

    启用USB功能
    Signed-off-by: MaxDYi <1575966435@QQ.com>

--- 2.Firmware/eHorglass/Core/Src/main.c
+++ 2.Firmware/eHorglass/Core/Src/main.c
@@ -120,7 +120,7 @@ int main(void)
     MX_SPI1_Init();
     MX_SPI2_Init();
     MX_USART1_UART_Init();
-    //MX_USB_DEVICE_Init();
+    MX_USB_DEVICE_Init();
     MX_ADC1_Init();
     MX_TIM3_Init();
     /* USER CODE BEGIN 2 */
